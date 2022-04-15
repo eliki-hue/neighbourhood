4de0366837a34c3ca35cb94e1e6f798aab70c2c5
ELIJAH KIRAGU <eliki13720@gmail.com>
2022-04-15 23:27:50 +0300

    added Profile model

--- myNeighbourhood/models.py
+++ myNeighbourhood/models.py
@@ -1,3 +1,25 @@
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
-
+from django.contrib.auth.models import User 
+from cloudinary.models import CloudinaryField
 # Create your models here.
+
+class Profile(models.Model):
+    username = models.CharField(max_length=20)
+    useremail = models.EmailField(max_length=30)
+    userage = models.CharField(max_length=2)
+    bio = models.CharField(max_length=100)
+    profile_image = CloudinaryField('images', null=True)
+    user_password = models.CharField(max_length=15)
+    def __str__(self):
+        return self.username
+
+    def save_profile(self):
+        self.save()
+
+    def delete_profile(self,username):
+        to_delete= Profile.objects.filter(username=username).delete()
+    
+    def update_profile(self,old_user,new_user):
+        Profile.objects.filter(username=old_user).update(name=new_user)
+        self.save()
