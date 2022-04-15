f16cd301dbe2c4ae37f5a40884bde6ef12e94ddd
ELIJAH KIRAGU <eliki13720@gmail.com>
2022-04-15 23:43:13 +0300

    added neighbourhood model

--- myNeighbourhood/models.py
+++ myNeighbourhood/models.py
@@ -23,3 +23,25 @@ class Profile(models.Model):
     def update_profile(self,old_user,new_user):
         Profile.objects.filter(username=old_user).update(name=new_user)
         self.save()
+
+class NeighbourHood(models.Model):
+    name = models.CharField(max_length=50)
+    location = models.CharField(max_length=60)
+    admin = models.ForeignKey("Profile", on_delete=models.CASCADE, related_name='hood')
+    hood_logo =  CloudinaryField('image')
+    description = models.TextField()
+    health_tell = models.IntegerField(null=True, blank=True)
+    police_number = models.IntegerField(null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.name} hood'
+
+    def create_neighborhood(self):
+        self.save()
+
+    def delete_neighborhood(self):
+        self.delete()
+
+    @classmethod
+    def find_neighborhood(cls, neighborhood_id):
+        return cls.objects.filter(id=neighborhood_id)
