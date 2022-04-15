9ec62976e0fcfd03311293272914cb7cefee2a3f
ELIJAH KIRAGU <eliki13720@gmail.com>
2022-04-15 22:21:23 +0300

    added registration, login and logout urls

--- neighbourhood/urls.py
+++ neighbourhood/urls.py
@@ -13,9 +13,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+from xml.etree.ElementInclude import include
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.contrib.auth import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('myNeighbourhood.urls')),
+    path('accounts/', include('registration.backends.simple.urls')),
+    path('logout/', views.LogoutView.as_view(), {"next_page": '/'}), 
+    path('login', views.redirect_to_login, {"next_page": '/'}),
+    path('accounts/', include('django.contrib.auth.urls')),
+
 ]
