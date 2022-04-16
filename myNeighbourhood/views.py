0fa308236368a534feed34a5a69b88b0b94c8511
ELIJAH KIRAGU <eliki13720@gmail.com>
2022-04-16 17:00:41 +0300

    added profile and profile display views

--- myNeighbourhood/views.py
+++ myNeighbourhood/views.py
@@ -17,3 +17,45 @@ def home(request):
     return render(request,'index.html',{'neighbourhood': neighbourHood, 'message': message})
 
 
+@login_required(login_url='/accounts/login/')
+def profile_update(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, request.FILES)
+        if form.is_valid():
+            profile= Profile.objects.filter(username=current_user)
+            print(profile)
+            if profile:
+                print('profile exist')
+                username = current_user
+                useremail=form.cleaned_data['useremail']
+               
+                userage=form.cleaned_data['userage']
+                profile_image=form.cleaned_data['profile_image']
+                AuthenticationError=form.cleaned_data['AuthenticationError']
+                Profile.objects.filter(username=current_user).update(useremail=useremail, userage=userage,profile_image=profile_image,AuthenticationError=AuthenticationError)
+            else:
+                print('profile does not exist')
+                profile=form.save(commit=False)
+                profile.username= current_user
+                profile.save()
+
+            message='saved successfuly'
+            # profile_display(request)
+            return redirect(profile_display)
+    
+            
+    else:
+        form = ProfileForm()
+        
+    return render(request, 'profile.html',{'form':form})
+       
+       
+@login_required(login_url='/accounts/login/')
+def profile_display(request):
+
+    current_user = request.user
+    profile= Profile.objects.filter(username=current_user)
+   
+
+    return render(request, 'profiledisplay.html',{'profile':profile})
