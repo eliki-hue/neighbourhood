47812873971036a28b2b3fe3f4bafeb7e42f2464
ELIJAH KIRAGU <eliki13720@gmail.com>
2022-04-16 17:16:23 +0300

    added urls

--- myNeighbourhood/urls.py
+++ myNeighbourhood/urls.py
@@ -4,17 +4,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns=[
-    # path('', views.home, name ='home'),
-    # path('my_profile', views.profile_display, name='profile_display'),
-    # path('update_profile', views.profile_update, name='profile_update'),
-    # path('my_post', views.add_post, name='my_post'),
-    # path('add_comment', views.add_comment, name='add_comment'),
-    # path('signup', views.signup, name='signup'),
-    # # path("like/<int:id>",views.project_like,name='project_like'),
-    # path("details/<int:pk>",views.project_details,name='project_details'),
-    # path('rate/', views.rate_project, name='rate_project' ),
-    # path('api/projects', views.ProjectList.as_view()),
-    # path('api/project/project-id/<int:pk>[0-9]+)', views.ProfileDescription.as_view()),
-    # path('search/', views.search_results, name='search_results'),
-    # path('rate/<int:pk>', views.rate, name='rate')
+    path('home', views.home, name ='home'),
+    path('my_profile', views.profile_display, name='profile_display'),
+    path('update_profile', views.profile_update, name='profile_update'),
+    path('my_post', views.add_post, name='my_post'),
+    path('add_business', views.add_comment, name='add_business'),
+    path('add_neighbourhood', views.add_comment, name='add_neighbourhood'),
+    
+    
 ]
